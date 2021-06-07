141653126a68660c38425c4ac3156bd65bbb026d
Verao <nikita-zanchenko@mail.ru>
2021-06-07 04:56:42 +0300

    all files ready to deploy

--- deployments.py
+++ deployments.py
@@ -1,11 +1,18 @@
 import values
-sudo curl  https://download.docker.com/linux/centos/docker-ce.repo -o /etc/yum.repos.d/docker-ce.repo
-sudo yum makecache
-sudo dnf -y  install docker-ce --nobest
-sudo systemctl enable --now docker
-sudo usermod -aG docker $USER
+#Установка curl & wget on the server
 sudo yum -y install curl
 sudo yum -y install wget
+#Установка Docker репозиторий
+sudo curl  https://download.docker.com/linux/centos/docker-ce.repo -o /etc/yum.repos.d/docker-ce.repo
+#Обновление кэша
+sudo yum makecache
+#Установка Docker
+sudo dnf -y  install docker-ce --nobest
+#Запуск Docker-compose при включении сервера
+sudo systemctl enable --now docker
+#Добавление пользователя докер в группу
+sudo usermod -aG docker $USER
+#Установка docker-compose
 curl -s https://api.github.com/repos/docker/compose/releases/latest \
   | grep browser_download_url \
   | grep docker-compose-Linux-x86_64 \
@@ -13,8 +20,13 @@ curl -s https://api.github.com/repos/docker/compose/releases/latest \
   | wget -qi -
 chmod +x docker-compose-Linux-x86_64
 sudo mv docker-compose-Linux-x86_64 /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo chgrp docker /usr/local/bin/docker-compose
+sudo chmod 750 /usr/local/bin/docker-compose
 sudo curl -L https://raw.githubusercontent.com/docker/compose/master/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
 source /etc/bash_completion.d/docker-compose
+#Установка Git и переход в ветку master для запуливания файлов с гит
 sudo yum install git
-git clone https://github.com/Vera0/IVCGB.git
+sudo git init
 git checkout master
+git pull https://github.com/Vera0/IVCGB.git
