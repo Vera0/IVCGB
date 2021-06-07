8f4bbfda3c3ff99bfd34e933c208097780c0ec25
Verao <nikita-zanchenko@mail.ru>
2021-06-07 03:04:58 +0300

    all files ready to deploy

--- deployments.py
+++ deployments.py
@@ -1,1 +1,18 @@
 import values
+sudo curl  https://download.docker.com/linux/centos/docker-ce.repo -o /etc/yum.repos.d/docker-ce.repo
+sudo yum makecache
+sudo dnf -y  install docker-ce --nobest
+sudo systemctl enable --now docker
+sudo usermod -aG docker $USER
+sudo yum -y install curl
+sudo yum -y install wget
+curl -s https://api.github.com/repos/docker/compose/releases/latest \
+  | grep browser_download_url \
+  | grep docker-compose-Linux-x86_64 \
+  | cut -d '"' -f 4 \
+  | wget -qi -
+chmod +x docker-compose-Linux-x86_64
+sudo mv docker-compose-Linux-x86_64 /usr/local/bin/docker-compose
+sudo curl -L https://raw.githubusercontent.com/docker/compose/master/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
+source /etc/bash_completion.d/docker-compose
+sudo yum install git
