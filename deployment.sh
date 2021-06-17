0c73f168919c29665b8b24102a05e48b92f7996c
Nikita <31665239+Vera0@users.noreply.github.com>
2021-06-17 11:36:26 +0300

    Update and rename deployments.py to deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 #Установка curl & wget on the server
 yum -y update
-yum install net-tools
-yum install bind-utils
+yum -y install net-tools
+yum -y install bind-utils
 yum -y install curl
 yum -y install wget
 #Установка Docker репозитория
@@ -13,25 +14,16 @@ dnf -y  install docker-ce --nobest
 #Запуск Docker при включении сервера
 systemctl enable --now docker
 #Добавление пользователя в группу Docker
-usermod -aG docker $USER
+usermod -aG docker $(whoami)
 #Установка docker-compose
-curl -s https://api.github.com/repos/docker/compose/releases/latest \
-  | grep browser_download_url \
-  | grep docker-compose-Linux-x86_64 \
-  | cut -d '"' -f 4 \
-  | wget -qi -
-#Добавление прав для бинарного файла Docker-compose
-chmod +x docker-compose-Linux-x86_64
-#Копируем бинарник в /usr/local/bin с изменением названия на docker-compose
-mv docker-compose-Linux-x86_64 /usr/local/bin/docker-compose
-#Даем ему права
+curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#Добавление прав для бинарного файла Docker-compose и создание симлинка
 chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 #Перемещаем бинарник docker-compose в группу docker
 chgrp docker /usr/local/bin/docker-compose
 #Даем ему права 750 (only User/owner and group can read&execute)
 chmod 750 /usr/local/bin/docker-compose
-curl -L https://raw.githubusercontent.com/docker/compose/master/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
-source /etc/bash_completion.d/docker-compose
 #Установка Git и переход в ветку master для запуливания файлов с гит
 yum -y install git
 git init
